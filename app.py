d8d534154679603170dafd3267400713faf74b1a
kgrewe <kgrewe@outlook.com>
2022-07-20 21:47:41 -0500

    7/20 Commit

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from student import Student #Imports only the student function from that student.py file
 
 #Object (The Actual Student)
-student1 = Student("Jim", "Business", 3.5, False)
-student2 = Student("Jill", "Comp Sci", 2.9, False)
+#student1 = Student("Jim", "Business", 3.5, False)
+#student2 = Student("Jill", "Comp Sci", 2.9, False)
+student_info = input("Enter the student's name, major, their GPA: ")
 
 
-print(student2.on_honor_roll())
+
+# While Loop example
+# guess_count = 0
+# while guess_count < 3:
+#     student_guess = input("Enter a student's name: ")
+#     guess_count += 1
+#     if student_guess == student2.name:
+#         print(student2.name, student2.major, student2.gpa, student2.on_honor_roll())
+#         break
+#     elif student_guess == student1.name:
+#         print(student1.name, student1.major, student1.gpa, student1.on_honor_roll())
+#         break
+#     else:
+#         print("Sorry, that student does not exist.")
+# else:
+#     print("Too many attempts, please try again later.")
--- student.py
+++ student.py
@@ -1,10 +1,10 @@
 #Defining what student is
 class Student:
-    def __init__(self, name, major, gpa, is_on_probation):
+    def __init__(self, name, major, gpa):
         self.name = name #Name of the Student object will = Name that we pass in
         self.major = major #Name of the Student major = Name of the major that we pass in
         self.gpa = gpa
-        self.is_on_probation = is_on_probation
+        
 
     def on_honor_roll(self):
         if self.gpa >= 3.5:
@@ -13,3 +13,8 @@ class Student:
             return False
 
 
+    def add_to_list(self):
+        students = []
+        major = []
+        gpa = []
+        return("Data saved successfully")
